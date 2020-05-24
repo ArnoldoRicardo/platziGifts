2856cb8e035f18aa7e1ca8a77f427f1f5d831bc7
ArnoldoRicardo <anyback@gmail.com>
2020-05-23 19:36:24 -0500

    404 y front-page

--- front-page.php
+++ front-page.php
@@ -10,6 +10,38 @@
 
         <?php }
 }?>
+
+<div class="lista-productos my-5">
+    <h2 class="text-center">PRODUCTOS</h2>
+    <div class="row">
+    <?php 
+    
+    $args = array(
+        'post_type'     => 'productos',
+        'post_per_page' => -1,
+        'order'         => 'ASC', 
+        'orderby'       => 'title',
+    );
+
+    $productos = new WP_Query($args);
+
+    if($productos->have_posts()){
+        while($productos->have_posts()){
+            $productos->the_post();
+        ?>
+            <div class="col-4">
+                <figure>
+                    <?php the_post_thumbnail( 'large' ); ?>
+                </figure>
+                    <h4 class="my-3 text-center">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h4>
+            </div>
+        <?php }
+    } else { echo 'no funciona'; }
+    ?>
+    </div>
+</div>
 </main>
 
 <?php get_footer()?>
